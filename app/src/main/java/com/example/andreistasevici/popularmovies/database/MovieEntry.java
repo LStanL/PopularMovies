46e4ff10bf63e1c5dff5628534d147783c983817
Andrei Stasevici <andreistasevici@gmail.com>
2018-08-26 18:12:26 -0700

    Creating MovieEntry Entity

---
+++ app/src/main/java/com/example/andreistasevici/popularmovies/database/MovieEntry.java
@@ -0,0 +1,56 @@
+package com.example.andreistasevici.popularmovies.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by andreistasevici on 8/26/18.
+ */
+
+@Entity(tableName = "movie")
+public class MovieEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private boolean isFavorite;
+
+    //creating 2 constructors
+    //first one can be used by us,
+    //but has to be ignored, since room doesn't support multiple constructors
+    public MovieEntry(String name, boolean isFavorite) {
+        this.name = name;
+        this.isFavorite = isFavorite;
+    }
+
+    //second constructor is used by room
+    public MovieEntry(int id, String name, boolean isFavorite) {
+        this.id = id;
+        this.name = name;
+        this.isFavorite = isFavorite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+}
