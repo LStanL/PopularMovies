19c3d994321efe6df791d8cf7d248a92e613d3f8
Andrei Stasevici <andreistasevici@gmail.com>
2018-09-03 00:06:19 -0700

    Add logging for retrofit network requests

--- app/src/main/java/com/example/andreistasevici/popularmovies/network/RetrofitClientInstance.java
+++ app/src/main/java/com/example/andreistasevici/popularmovies/network/RetrofitClientInstance.java
@@ -2,6 +2,8 @@ package com.example.andreistasevici.popularmovies.network;
 
 import com.example.andreistasevici.popularmovies.utils.Constants;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,10 +17,17 @@ public class RetrofitClientInstance {
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
+        //add interceptor for logging network requests
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(Constants.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
 
